d6f6ae11011fec5dcd26a130e08f5ee3a62db7cd
kKsk <alonzomax1210@gmail.com>
2023-10-09 22:00:35 +0800

    feat: Image Zoom In

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,32 @@
-// import './styles/index.css' // 引入自定义样式:不生效,需要在组件下方引入
-
 import DefaultTheme from "vitepress/theme";
+
+// 图片缩放部分导入
+import { onMounted, watch, nextTick } from 'vue';
+import { useRoute } from 'vitepress';
+import mediumZoom from 'medium-zoom';
+
 import "./styles/index.css";
 
 export default {
   ...DefaultTheme,
   enhanceApp(ctx) {
+    
     // 继承默认主题
     DefaultTheme.enhanceApp(ctx);
+  },
+
+  // 图片缩放
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+        mediumZoom('.main img', { background: 'var(--vp-c-bg)' });
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
   },
 };
